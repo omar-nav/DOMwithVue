cf726061442e2d65088a63d264812bbcaf69b90d
omar-nav <omar.navarro@g.austincc.edu>
2018-12-26 13:46:34 -0600

    understanding a vue instance

--- main.js
+++ main.js
@@ -1,59 +1,35 @@
-var data = {
-  title: "The vueJS Instance",
-  showParagraph: false
-}
-
-Vue.component('hello', {
-  template: '<h1>Hello!</h1>'
-});
-
-var vm1 = new Vue({
-
-  data: data,
-  methods: {
-    show: function () {
-      this.showParagraph = true;
-      this.updateTitle('The vueJS instance updated')
-      this.$refs.myButton.innerText = 'Test';
-    },
-    updateTitle: function (title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function () {
-      return this.title.toLowerCase();
-    }
-  }
-});
-
-vm1.$mount('#app1');
-
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-setTimeout(function () {
-  vm1.title = 'Changed by timer';
-  vm1.show();
-}, 2000);
-
-
-var vm2 = new Vue({
-  el: "#app2",
+new Vue({
+  el: '#app',
   data: {
-    title: 'The second instance'
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function () {
+    console.log('beforeCreate()');
+  },
+  created: function () {
+    console.log('created()');
+  },
+  beforeMount: function () {
+    console.log('beforeMount()');
+  },
+  mounted: function () {
+    console.log('mounted')
+  },
+  beforeUpdate: function () {
+    console.log('beforeUpdate()');
+  },
+  updated: function () {
+    console.log('updated()');
+  },
+  beforeDestroy: function () {
+    console.log('beforeDestroy()');
+  },
+  destroyed: function () {
+    console.log('destroyed()');
   },
   methods: {
-    onChange: function () {
-      vm1.title = 'Changed!';
+    destroy: function () {
+      this.$destroy();
     }
   }
-});
-
-var vm3 = new Vue({
-  el: '.hello',
-  template: '<h1>Sweet! So if its a tag name -> no hash tag</h1>'
-});
-
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
+})
